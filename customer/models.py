e7f52567a96f7041070d43482c30619d7db3af87
mohammad <mohammadnasimi6803@gmail.com>
2022-02-04 23:39:51 +0330

    add customer model

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from core.models import BaseModel
+
 
 # Create your models here.
+
+class Customer(BaseModel):
+    first_name = models.CharField(max_length=150, verbose_name='first_name')
+    last_name = models.CharField(max_length=150, verbose_name='last_name')
+    email = models.EmailField(max_length=250)
+    Password = models.CharField(max_length=50)
