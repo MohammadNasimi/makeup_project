03b048cf7a48e6333b3ffd984547c83e6af7693a
mohammad <mohammadnasimi6803@gmail.com>
2022-02-04 23:45:46 +0330

    add model service

--- service/models.py
+++ service/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from core.models import BaseModel
+
 
 # Create your models here.
+class Service(BaseModel):
+    name = models.CharField(max_length=40)
+    time = models.PositiveIntegerField(null=False, default=1, verbose_name='Time_Service')
+    cost = models.PositiveIntegerField(null=False, verbose_name='Price_Service')
