81b92a364dfa3a8c4563b69963a73908fecfeaf8
mohammad <mohammadnasimi6803@gmail.com>
2022-02-04 23:39:37 +0330

    add app in setting

--- mini_project_dj/settings.py
+++ mini_project_dj/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'customer',
+    'booking',
+    'Agent',
+    'service'
 ]
 
 MIDDLEWARE = [
