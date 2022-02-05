0b7656f196428c18ab2e195f84f6c67546dd0e7f
mohammad <mohammadnasimi6803@gmail.com>
2022-02-05 23:11:07 +0330

    add views customer

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.edit import CreateView
+from customer.models import Customer
+
+
+class CustomerCreateView(CreateView):
+    model = Customer
+    fields = '__all__'
