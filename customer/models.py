ffb4657ac73eb1f28e41307f80d0ab31c211bc90
mohammad <mohammadnasimi6803@gmail.com>
2022-02-06 01:49:33 +0330

    change password field null=true

--- customer/models.py
+++ customer/models.py
@@ -8,4 +8,4 @@ class Customer(BaseModel):
     first_name = models.CharField(max_length=150, verbose_name='first_name')
     last_name = models.CharField(max_length=150, verbose_name='last_name')
     email = models.EmailField(max_length=250)
-    Password = models.CharField(max_length=50)
+    Password = models.CharField(max_length=50, null=True, blank=True)
