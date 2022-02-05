f37816eefef1817661efc0a7174011d4904dc0e6
mohammad <mohammadnasimi6803@gmail.com>
2022-02-06 02:54:18 +0330

    add  Agent views

--- Agent/views.py
+++ Agent/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from Agent.models import Agent, Weekend
+
+
+class AgentCreateView(CreateView):
+    model = Agent
+    fields = ['first_name', 'last_name', 'descriptions', 'experience_year', 'count_service', 'image']
+    template_name = 'Agent/create_Agent.html'
+    success_url = reverse_lazy('Agent:create_Agent')
+
+
+class WeekendCreateView(CreateView):
+    model = Weekend
+    fields = ['Agent', 'start_time', 'end_time']
+    template_name = 'Agent/create_Weekend.html'
+    success_url = reverse_lazy('service:create')
