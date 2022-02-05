51a07a3bd9e615c2fd450ad77e04580ac4bbd10a
mohammad <mohammadnasimi6803@gmail.com>
2022-02-06 02:06:18 +0330

    add views service

--- service/views.py
+++ service/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from customer.models import Customer
+
+
+class ServiceCreateView(CreateView):
+    model = Customer
+    fields = ['name', 'time', 'cost']
+    template_name = 'customer/create_customer.html'
+    success_url = reverse_lazy('customer:create')
