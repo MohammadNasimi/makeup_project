47ea209b5775b78befbaf78fb420325711422ea7
mohammad <mohammadnasimi6803@gmail.com>
2022-02-05 23:35:36 +0330

    change name url customer

--- mini_project_dj/urls.py
+++ mini_project_dj/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('create/', include('customer.urls')),
+                  path('customer/', include('customer.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
