0a1758ffdf79e2fbca50ca041f3c464746bf77b7
mohammad <mohammadnasimi6803@gmail.com>
2022-02-07 00:05:05 +0330

    form create booking

---
+++ booking/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from booking.models import Booking
+
+
+class AgentForm(forms.ModelForm):
+    class Meta:
+        model: Booking
+        fields = ['service', 'count_adult', 'count_child', 'start_time', 'agent', 'customer']
