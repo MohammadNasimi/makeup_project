abe6f6c511d936c114ef4e890d03e5af189e531f
mohammad <mohammadnasimi6803@gmail.com>
2022-02-07 00:05:35 +0330

    views create booking

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from booking.models import Booking
+
 
 # Create your views here.
+class BookingCreateView(CreateView):
+    model = Booking
+    fields = ['service', 'count_adult', 'count_child', 'start_time', 'agent', 'customer']
+    template_name = 'Booking/create_booking.html'
+    success_url = reverse_lazy('booking:create_booking')
