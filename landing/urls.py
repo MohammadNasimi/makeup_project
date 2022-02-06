e37b62d0221e53e6aa640bd5d6143b578bb52e7e
mohammad <mohammadnasimi6803@gmail.com>
2022-02-07 01:33:02 +0330

    url contact and about in views

--- landing/urls.py
+++ landing/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'landing'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='Home'),
+    path('contact_us/', views.ContactusView.as_view(), name='contact_us'),
+    path('about_us/', views.AboutView.as_view(), name='about_us'),
 ]
