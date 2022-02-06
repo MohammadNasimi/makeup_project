0d583126dde078deb3bac68af0d53609f1ff913b
mohammad <mohammadnasimi6803@gmail.com>
2022-02-07 01:33:16 +0330

    url contact and about in landing

--- landing/views.py
+++ landing/views.py
@@ -6,3 +6,11 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "landing/base.html"
+
+
+class ContactusView(TemplateView):
+    template_name = "landing/contact_us.html"
+
+
+class AboutView(TemplateView):
+    template_name = "landing/about_us.html"
