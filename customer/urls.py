b23c6d41640ea0b7f227882848d44c9cc3b624f0
mohammad <mohammadnasimi6803@gmail.com>
2022-02-08 20:08:24 +0330

    add url read update delete  customer

--- customer/urls.py
+++ customer/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'customer'
 urlpatterns = [
     path('create/', views.CustomerCreateView.as_view(), name='create'),
+    path('list/', views.CustomerListView.as_view(), name='list'),
+    path('update/<int:pk>', views.CustomerUpdateView.as_view(), name='update'),
+    path('Delete/<int:pk>', views.CustomerDeleteView.as_view(), name='delete'),
 ]
