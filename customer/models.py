4bad4f86302979b60e519612a1e40451978544ad
mohammad <mohammadnasimi6803@gmail.com>
2022-02-08 21:14:24 +0330

    add str in customer model

--- customer/models.py
+++ customer/models.py
@@ -9,3 +9,6 @@ class Customer(BaseModel):
     last_name = models.CharField(max_length=150, verbose_name='last_name')
     email = models.EmailField(max_length=250)
     Password = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
