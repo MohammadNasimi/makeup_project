21460a00ea372ee5381af8011f4ba1d45b8026ce
mohammad <mohammadnasimi6803@gmail.com>
2022-02-08 23:24:51 +0330

    change name

--- booking/forms.py
+++ booking/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from booking.models import Booking
 
 
-class AgentForm(forms.ModelForm):
+class BookingForm(forms.ModelForm):
     class Meta:
         model: Booking
         fields = ['service', 'count_adult', 'count_child', 'start_time', 'agent', 'customer']
