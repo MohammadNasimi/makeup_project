d0674ef3b1da09c48582c1ad7091e4000840ee45
mohammad <mohammadnasimi6803@gmail.com>
2022-02-08 19:22:11 +0330

    add book listview url

--- booking/urls.py
+++ booking/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'Booking'
 urlpatterns = [
     path('create_booking/', views.BookingCreateView.as_view(), name='create_booking'),
+    path('list_booking/', views.BookingListView.as_view(), name='list_booking'),
 ]
