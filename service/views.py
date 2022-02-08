4951322a09985f39b75ad42cb40366b694db8dd4
mohammad <mohammadnasimi6803@gmail.com>
2022-02-08 05:07:02 +0330

    add service update and delete views

--- service/views.py
+++ service/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView,ListView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from service.models import Service
 
 
@@ -18,3 +18,15 @@ class ServiceListView(ListView):
     fields = ['name', 'time', 'cost']
     template_name = 'Service/list_Service.html'
 
+
+class ServiceUpdateView(UpdateView):
+    model = Service
+    fields = ['name', 'time', 'cost']
+    template_name = 'Service/update_Service.html'
+    success_url = reverse_lazy('service:list')
+
+
+class ServiceDeleteView(DeleteView):
+    model = Service
+    template_name = 'Service/delete_Service.html'
+    success_url = reverse_lazy('service:list')
