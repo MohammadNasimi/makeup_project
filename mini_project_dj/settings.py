97f324c718e81a32da192e09bfbc6996a1ec6b18
mohammad <mohammadnasimi6803@gmail.com>
2022-02-08 20:09:25 +0330

    add url read update delete  customer

--- mini_project_dj/settings.py
+++ mini_project_dj/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'admin_volt.apps.AdminVoltConfig',
+    # 'admin_volt.apps.AdminVoltConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ templates/customer/customer/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from customer.models import Customer
+
+
+class CustomerCreateView(CreateView):
+    model = Customer
+    fields = ['first_name', 'last_name', 'email', 'Password']
+    template_name = 'customer/create_customer.html'
+    success_url = reverse_lazy('customer:create')
+
+
+class CustomerListView(ListView):
+    model = Customer
+    fields = ['first_name', 'last_name', 'email', 'Password']
+    template_name = 'customer/list_Customer.html'
+
+
+class CustomerUpdateView(UpdateView):
+    model = Customer
+    fields = ['first_name', 'last_name', 'email', 'Password']
+    template_name = 'customer/update_Customer.html'
+    success_url = reverse_lazy('Customer:list')
+
+
+class CustomerDeleteView(DeleteView):
+    model = Customer
+    template_name = 'customer/delete_Customer.html'
+    success_url = reverse_lazy('Customer:list')
