0b3af0f2bfe6f324c04616735601ba1bbf0087b7
mohammad <mohammadnasimi6803@gmail.com>
2022-02-08 20:08:35 +0330

    add views read update delete  customer

--- customer/views.py
+++ customer/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from customer.models import Customer
 
 
@@ -11,3 +11,22 @@ class CustomerCreateView(CreateView):
     fields = ['first_name', 'last_name', 'email', 'Password']
     template_name = 'customer/create_customer.html'
     success_url = reverse_lazy('customer:create')
+
+
+class CustomerListView(ListView):
+    model = Customer
+    fields = ['first_name', 'last_name', 'email', 'Password']
+    template_name = 'customer/list_customer.html'
+
+
+class CustomerUpdateView(UpdateView):
+    model = Customer
+    fields = ['first_name', 'last_name', 'email', 'Password']
+    template_name = 'customer/update_customer.html'
+    success_url = reverse_lazy('customer:list')
+
+
+class CustomerDeleteView(DeleteView):
+    model = Customer
+    template_name = 'customer/delete_customer.html'
+    success_url = reverse_lazy('customer:list')
