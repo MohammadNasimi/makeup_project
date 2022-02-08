d4812e83e78a68edddf4f94ba63fae9b61442112
mohammad <mohammadnasimi6803@gmail.com>
2022-02-08 20:37:31 +0330

    add views Agent list delete update

--- Agent/views.py
+++ Agent/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DeleteView, UpdateView
 from Agent.models import Agent, Weekend
 
 
@@ -11,6 +11,25 @@ class AgentCreateView(CreateView):
     fields = ['first_name', 'last_name', 'descriptions', 'experience_year', 'count_service', 'image']
     template_name = 'Agent/create_Agent.html'
     success_url = reverse_lazy('Agent:create_Agent')
+
+
+class AgentListView(ListView):
+    model = Agent
+    fields = ['first_name', 'last_name', 'descriptions', 'experience_year', 'count_service', 'image']
+    template_name = 'Agent/list_agent.html'
+
+
+class AgentUpdateView(UpdateView):
+    model = Agent
+    fields = ['first_name', 'last_name', 'descriptions', 'experience_year', 'count_service', 'image']
+    template_name = 'Agent/update_agent.html'
+    success_url = reverse_lazy('Agent:list_agent')
+
+
+class AgentDeleteView(DeleteView):
+    model = Agent
+    template_name = 'Agent/delete_agent.html'
+    success_url = reverse_lazy('Agent:list_agent')
 
 
 class WeekendCreateView(CreateView):
