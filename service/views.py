75a22ac926037c02a826172a52faef0dddb8bd93
mohammad <mohammadnasimi6803@gmail.com>
2022-02-08 04:33:19 +0330

    add list view

--- service/views.py
+++ service/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView,ListView
 from service.models import Service
 
 
@@ -11,3 +11,10 @@ class ServiceCreateView(CreateView):
     fields = ['name', 'time', 'cost']
     template_name = 'Service/create_Service.html'
     success_url = reverse_lazy('service:create')
+
+
+class ServiceListView(ListView):
+    model = Service
+    fields = ['name', 'time', 'cost']
+    template_name = 'Service/list_Service.html'
+
