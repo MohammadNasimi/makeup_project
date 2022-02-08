8cd105b3296c1ae44b28966c7d25d36716e28fe7
mohammad <mohammadnasimi6803@gmail.com>
2022-02-08 05:06:44 +0330

    add service update and delete

--- service/urls.py
+++ service/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'service'
 urlpatterns = [
     path('create/', views.ServiceCreateView.as_view(), name='create'),
+    path('list/', views.ServiceListView.as_view(), name='list'),
+    path('update/<int:pk>', views.ServiceUpdateView.as_view(), name='update'),
+    path('Delete/<int:pk>', views.ServiceDeleteView.as_view(), name='delete'),
 ]
