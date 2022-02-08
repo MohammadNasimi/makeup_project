3f6e5edda888f4aae87af9098b1ff93555140119
mohammad <mohammadnasimi6803@gmail.com>
2022-02-08 20:34:03 +0330

    add url Agent list delete update

--- Agent/urls.py
+++ Agent/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'Agent'
 urlpatterns = [
     path('create_Agent/', views.AgentCreateView.as_view(), name='create_Agent'),
-    path('list_agent/', views.CustomerListView.as_view(), name='list_agent'),
-    path('update_agent/<int:pk>', views.CustomerUpdateView.as_view(), name='update_agent'),
-    path('Delete_agent/<int:pk>', views.CustomerDeleteView.as_view(), name='delete_agent'),
+    path('list_agent/', views.AgentListView.as_view(), name='list_agent'),
+    path('update_agent/<int:pk>', views.AgentUpdateView.as_view(), name='update_agent'),
+    path('Delete_agent/<int:pk>', views.AgentDeleteView.as_view(), name='delete_agent'),
     path('create_weekend/', views.WeekendCreateView.as_view(), name='create_weekend'),
 ]
