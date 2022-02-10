984584e6b73663af1ea1d6a3bfa92e141becc4c8
mohammad <mohammadnasimi6803@gmail.com>
2022-02-10 23:40:35 +0330

    form book

--- booking/models.py
+++ booking/models.py
@@ -7,14 +7,14 @@ from service.models import Service
 
 # Create your models here.
 class Booking(BaseModel):
-    service = models.ForeignKey(Service, on_delete=models.RESTRICT, verbose_name='service')
+    service = models.ForeignKey(Service, on_delete=models.RESTRICT)
     count_adult = models.PositiveIntegerField(default=0)
     count_child = models.PositiveIntegerField(default=0)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     cost_final = models.PositiveIntegerField(default=0)
-    agent = models.ForeignKey(Agent, on_delete=models.RESTRICT, verbose_name='Agent')
-    customer = models.ForeignKey(Customer, on_delete=models.RESTRICT, verbose_name='Customer')
+    agent = models.ForeignKey(Agent, on_delete=models.RESTRICT)
+    customer = models.ForeignKey(Customer, on_delete=models.RESTRICT)
 
     def __str__(self):
         return f'{self.service} {self.cost_final}'
