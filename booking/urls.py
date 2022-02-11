99671eefde61b2b56f057cbd6d605e6195618be7
mohammad <mohammadnasimi6803@gmail.com>
2022-02-11 22:11:28 +0330

    change url booking create

--- booking/urls.py
+++ booking/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'Booking'
 urlpatterns = [
-    path('create_booking/', views.BookingCreateView.as_view(), name='create_booking'),
+    path('create_booking/', views.BookingView.as_view(), name='create_booking'),
     path('list_booking/', views.BookingListView.as_view(), name='list_booking'),
 ]
