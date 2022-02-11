a323f27238fe869b3015f7d00a0b59f767be67b9
mohammad <mohammadnasimi6803@gmail.com>
2022-02-11 22:12:18 +0330

    change kind form

--- booking/forms.py
+++ booking/forms.py
@@ -2,7 +2,13 @@ from django import forms
 from booking.models import Booking
 
 
-class BookingForm(forms.ModelForm):
-    class Meta:
-        model: Booking
-        fields = ['service', 'count_adult', 'count_child', 'start_time', 'agent', 'customer']
+# class BookingForm(forms.ModelForm):
+#     class Meta:
+#         model = Booking
+#         fields = ['service', 'count_adult', 'count_child', 'start_time', 'Agent']
+class BookingForm(forms.Form):
+    service = forms.IntegerField(label='service', widget=forms.Select(attrs={'id': 'id_service'}))
+    adult_count = forms.IntegerField(label='Adult', initial=0, widget=forms.NumberInput(attrs={'min': '0'}))
+    child_count = forms.IntegerField(label='Child', initial=0, widget=forms.NumberInput(attrs={'min': '0'}))
+    start_time = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}, format='%Y-%m-%d'))
+    Agent = forms.IntegerField(label='agent', widget=forms.Select(attrs={'id': 'id_agent'}))
